a1c316f86340b7c8dad70fac9536a16feaabaa5d
Arthur <wangyn@qeek.cc>
2022-03-31 11:09:39 +0800

    Added the rainbow widget.

---
+++ lib/Widgets/rainbow.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Rainbow extends StatelessWidget {
+  const Rainbow({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
