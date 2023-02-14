5560b676af39fa06ab4f30e540694eca2271066d
Arthur <wangyn@qeek.cc>
2023-02-14 11:34:14 +0800

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_snippet/Common/MaterialAppUtil.dart';
 import 'package:flutter_snippet/Common/my_colors.dart';
-import 'package:flutter_snippet/learnAnnotation.g.dart';
 
 void main() {
   runApp(ProviderScope(child: createMaterialApp((settings) => MaterialPageRoute(builder: (_) => const MyHomePage()), {})));
@@ -27,11 +27,16 @@ class _MyHomePageState extends ConsumerState<MyHomePage> with SingleTickerProvid
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("突飞猛进"),
+    debugPrint("123");
+
+    return const Scaffold(
+      // appBar: AppBar(
+      //   title: const Text("突飞猛进"),
+      // ),
+      body: SafeArea(
+        maintainBottomViewPadding: false,
+        child: MyApp(),
       ),
-      body: const MyApp(),
     );
   }
 }
@@ -44,47 +49,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
-
-    var test = Wang();
-
-    var random = Random();
-    return CustomScrollView(
-      // physics: const AlwaysScrollableScrollPhysics(),
-      slivers: [
-        SliverAppBar(
-          actions: <Widget>[IconButton(onPressed: () => {}, icon: const Icon(Icons.add))],
-          expandedHeight: 200,
-          pinned: true,
-          stretch: true,
-          flexibleSpace: FlexibleSpaceBar(
-            title: const Text(
-              "First FlexibleSpace",
-              style: TextStyle(color: Colors.blue),
-            ),
-            background: Image.network(
-                "https://p3-passport.byteimg.com/img/user-avatar/af5f7ee5f0c449f25fc0b32c050bf100~180x180.awebp",
-                fit: BoxFit.cover),
-            stretchModes: const [
-              StretchMode.zoomBackground,
-              StretchMode.blurBackground,
-              StretchMode.fadeTitle
-            ],
-          ),
-        ),
-        SliverGrid.extent(
-          maxCrossAxisExtent: 150.0,
-          children: List.generate(
-              100,
-              (index) => Container(
-                    color: MyColors.randomColor(),
-                    margin: EdgeInsets.all(random.nextInt(30).toDouble()),
-                    child: const Text("测试下"),
-                  )),
-        ),
-      ],
+    return Container(
+      color: Colors.red,
+      width: 100,
+      height: 100,
     );
   }
 }
