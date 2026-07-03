4ce670570b7f7501f6b0f12db234c2a4ba49670f
Arthur <wangyn@qeek.cc>
2026-07-03 11:28:33 +0800

    添加一个单元测试

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_snippet/Widgets/bloc/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_snippet/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('BlocTest increments and decrements counter', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: BlocTest()));
 
-    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+
+    await tester.tap(find.byIcon(Icons.remove));
+    await tester.pump();
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
   });
 }
